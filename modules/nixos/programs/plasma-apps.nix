92626cd98250cb6e6a9e6abcf257d7a055186cdd
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2026-01-02 12:57:51 +0100

    Added krename to plasma apps

--- modules/nixos/programs/plasma-apps.nix
+++ modules/nixos/programs/plasma-apps.nix
@@ -4,26 +4,25 @@
         };
         config = lib.mkIf config.plasma-apps.enable {
                 environment.systemPackages = with pkgs; [ 
-                        kdePackages.elisa # Music player
-                        kdePackages.dragon # Video player
                         devtoolbox # Just some nicities
-                        # kdePackages.neochat # Matrix client
+                        gtranslator # PO translation editor
+                        kdePackages.dragon # Video player
+                        kdePackages.elisa # Music player
                         kdePackages.isoimagewriter # dd but Gnome
-                        parabolic # Media downloader
-                        kdePackages.okular # Pdf reader
+                        kdePackages.kapman # Pacman in all but name
+                        kdePackages.kbreakout
                         kdePackages.kolourpaint # Paint
+                        kdePackages.konquest
+                        kdePackages.okular # Pdf reader
+                        krename # Bulk rename tol
+                        parabolic # Media downloader
                         pdfarranger # Pdf arranger
                         pitivi # Video editor
                         rnote # Handdrawn note taking
-                        gtranslator # PO translation editor
-                        # Games
-                        kdePackages.kapman # Pacman in all but name
-                        kdePackages.kbreakout
-                        kdePackages.konquest
                 ];
                 environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
-                        pkgs.gst_all_1.gst-plugins-good
                         pkgs.gst_all_1.gst-plugins-bad
+                        pkgs.gst_all_1.gst-plugins-good
                         pkgs.gst_all_1.gst-plugins-ugly
                 ];
         };
