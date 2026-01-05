3cdf87d9098610a02b5a5c8a32b3b0c1e42803a6
Mathijs <79464596+CouldBeMathijs@users.noreply.github.com>
2026-01-05 14:33:03 +0100

    Disable fixup to make speedier

--- packages/gruvbox-plus-icons-git.nix
+++ packages/gruvbox-plus-icons-git.nix
@@ -1,45 +1,38 @@
 { lib, stdenvNoCC, gruvbox-icons, gtk3, plasma5Packages, gnome-icon-theme, hicolor-icon-theme }:
 
 stdenvNoCC.mkDerivation rec {
-        pname = "gruvbox-plus-icons-git";
-        version = gruvbox-icons.lastModifiedDate;
-        src = gruvbox-icons;
+  pname = "gruvbox-plus-icons-git";
+  version = gruvbox-icons.lastModifiedDate or "latest";
+  src = gruvbox-icons;
 
-        nativeBuildInputs = [ gtk3 ];
+  nativeBuildInputs = [ gtk3 ];
 
-        propagatedBuildInputs = [
-                plasma5Packages.breeze-icons
-                gnome-icon-theme
-                hicolor-icon-theme
-        ];
+  propagatedBuildInputs = [
+    plasma5Packages.breeze-icons
+    gnome-icon-theme
+    hicolor-icon-theme
+  ];
 
-        installPhase = ''
-  runHook preInstall
+  dontBuild = true;
+  dontConfigure = true;
 
-  # Copy the theme to a temporary, writable directory
-  cp -r Gruvbox-Plus-Dark $TMPDIR/
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/share/icons/Gruvbox-Plus-Dark
+    cp -r Gruvbox-Plus-Dark/* $out/share/icons/Gruvbox-Plus-Dark/
+    
+    # Generate cache here
+    gtk-update-icon-cache $out/share/icons/Gruvbox-Plus-Dark
+    runHook postInstall
+  '';
 
-  # Update the icon cache in the temporary directory
-  gtk-update-icon-cache $TMPDIR/Gruvbox-Plus-Dark
+  dontFixup = true;
 
-  # Copy the complete theme (with the cache file) to the output directory
-  mkdir -p $out/share/icons/
-  cp -r $TMPDIR/Gruvbox-Plus-Dark $out/share/icons/
-
-  runHook postInstall
-        '';
-        dontDropIconThemeCache = false;
-        dontBuild = true;
-        dontConfigure = true;
-
-        meta = {
-                description = "Icon pack for Linux desktops based on the Gruvbox color scheme";
-                homepage = "https://github.com/SylEleuth/gruvbox-plus-icon-pack";
-                license = lib.licenses.gpl3Only;
-                platforms = lib.platforms.linux;
-                maintainers = with lib.maintainers; [
-                        eureka-cpu
-                        Gliczy
-                ];
-        };
+  meta = with lib; {
+    description = "Icon pack for Linux desktops based on the Gruvbox color scheme";
+    homepage = "https://github.com/SylEleuth/gruvbox-plus-icon-pack";
+    license = licenses.gpl3Only;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ CouldBeMathijs ];
+  };
 }
